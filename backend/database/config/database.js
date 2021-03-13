1173c24fcbbc43557512a48e20553f0f81e951cc
Igor <ig.vp@hotmail.com>
2021-03-13 15:19:30 -0300

    initial test about DB

---
+++ backend/database/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "password",
+  database: "ecommerce",
+  define: {
+    timestamps: true,
+  },
+};
