fbdc484cfd8e5a54a0777df8b85ac57142abfdd3
Igor <ig.vp@hotmail.com>
2021-03-14 15:54:53 -0300

    added one more layer (services) to the project

--- backend/database/controller/UserController.js
+++ backend/database/controller/UserController.js
@@ -1,38 +1,29 @@
 const UserModel = require("../models/UserModel");
-const UtilsBcrypt = require("../../server/utils/bcrypt");
+const userService = require("../services/UserService");
 
 module.exports = {
   async createUser(req, res) {
-    const { name, email, password } = req.body;
-    const hashedPassword = await UtilsBcrypt.encrypt(password);
-    const user = await UserModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+    const user = await userService.createUser(req, res);
     return res.json(user);
   },
 
   async listUsers(req, res) {
-    const users = await UserModel.findAll();
+    const users = await userService.listUsers(req, res);
     return res.json(users);
   },
 
   async listUser(req, res) {
-    const user = await UserModel.findByPk(req.params.id);
+    const user = await userService.listUser(req, res);
     return res.json(user);
   },
 
   async updateUser(req, res) {
-    const { name, email } = req.body;
-    await UserModel.update({ name }, { where: { id: req.params.id } });
-    await UserModel.update({ email }, { where: { id: req.params.id } });
-    const updatedUser = await UserModel.findByPk(req.params.id);
+    const updatedUser = await userService.updateUser(req, res);
     return res.json(updatedUser);
   },
 
   async deleteUser(req, res) {
-    await UserModel.destroy({ where: { id: req.params.id } });
-    res.json({ menssage: "deleted" });
+    await userService.deleteUser(req, res);
+    return res.json({ menssage: `User id: ${req.params.id} deleted` });
   },
 };
---
+++ backend/database/services/UserService.js
@@ -0,0 +1,30 @@
+const UserModel = require("../models/UserModel");
+const UtilsBcrypt = require("../../server/utils/bcrypt");
+
+module.exports = {
+  async createUser(req, res) {
+    const { name, email, password } = req.body;
+    const hashedPassword = await UtilsBcrypt.encrypt(password);
+    const user = await UserModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return user;
+  },
+  async listUsers(req, res) {
+    return await UserModel.findAll();
+  },
+  async listUser(req, res) {
+    return (user = UserModel.findByPk(req.params.id));
+  },
+  async updateUser(req, res) {
+    const { name, email } = req.body;
+    await UserModel.update({ name }, { where: { id: req.params.id } });
+    await UserModel.update({ email }, { where: { id: req.params.id } });
+    return UserModel.findByPk(req.params.id);
+  },
+  async deleteUser(req, res) {
+    return UserModel.destroy({ where: { id: req.params.id } });
+  },
+};
