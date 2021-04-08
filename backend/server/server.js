1830fd1a95ea5851d164de6ebd57309f89c664a9
andradeigor <ig.vp@hotmail.com>
2021-04-08 17:43:32 -0300

    added the dinamic product home page

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const UserRouter = require("./routes/User");
 const AddressRouter = require("./routes/Address");
 const ProductsRouter = require("./routes/Products");
@@ -6,6 +7,7 @@ require("../database/index");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/", UserRouter);
 app.use("/users/", AddressRouter);
 app.use("/admin/products/", ProductsRouter);
--- web/src/components/CardDisplay/index.js
+++ web/src/components/CardDisplay/index.js
@@ -1,138 +1,15 @@
 import { CardDisplayContainer } from "./style";
 import Card from "../Card/index";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const CardDisplay = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 2,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 3,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 4,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 5,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 6,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 7,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 8,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 9,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 10,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 11,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 12,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 13,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 14,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 15,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-    {
-      id: 16,
-      title: "Gray T-Shirt",
-      price: "80,00",
-      subtitle: "Ou em até 12x sem juros",
-      imagePath:
-        "https://user-images.githubusercontent.com/21049910/113764970-0ed4a100-96f2-11eb-97ab-c4fab2bfb84d.png",
-    },
-  ];
-
+  const [data, SetData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/admin/products")
+      .then((res) => SetData(res.data));
+  }, []);
   return (
     <CardDisplayContainer>
       {data.map((product) => (
